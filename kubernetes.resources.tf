91d587c836fa15cdf6a18962db397a43ff68a6ee
Dan Petersen <dp@danitso.com>
2019-08-06 17:12:57 +0200

    Initial work on CCM support

--- kubernetes.resources.tf
+++ kubernetes.resources.tf
@@ -78,8 +78,6 @@ networking:
 apiServer:
   certSANs:
   - ${join("\n  - ", module.load_balancers.load_balancer_public_addresses)}
-
-  # https://kubernetes.io/docs/reference/command-line-tools-reference/kube-apiserver/
   extraArgs:
     max-requests-inflight: "1000"
     max-mutating-requests-inflight: "500"
@@ -87,13 +85,10 @@ apiServer:
     watch-cache-sizes: "persistentvolumeclaims#1000,persistentvolumes#1000"
 
 controllerManager:
-  # https://kubernetes.io/docs/reference/command-line-tools-reference/kube-controller-manager/
   extraArgs:
+    cloud-provider: "external"
+    cluster-cidr: "10.32.0.0/12"
     deployment-controller-sync-period: "50s"
-# scheduler:
-#   # https://kubernetes.io/docs/reference/command-line-tools-reference/kube-scheduler/
-#   extraArgs:
-#     address: 0.0.0.0
 EOT
     destination = "/tmp/config.new.yaml"
   }
@@ -127,6 +122,7 @@ resource "null_resource" "kubernetes_master_join" {
 
   provisioner "remote-exec" {
     inline = [
+      "echo 'KUBELET_EXTRA_ARGS=--cloud-provider=external' >> /etc/default/kubelet",
       "kubeadm join ${element(module.load_balancers.load_balancer_public_addresses, 0)}:6443 --token ${join(".", random_string.kubernetes_bootstrap_token.*.result)} --discovery-token-unsafe-skip-ca-verification --control-plane --certificate-key ${random_id.kubernetes_certificate_key.hex}",
     ]
   }
@@ -152,6 +148,49 @@ resource "null_resource" "kubernetes_network" {
     inline = [
       "export KUBECONFIG=/etc/kubernetes/admin.conf",
       "kubectl apply -f https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')",
+    ]
+  }
+}
+
+resource "null_resource" "kubernetes_cloud_controller" {
+  depends_on = [
+    "null_resource.kubernetes_master_join",
+  ]
+
+  connection {
+    type  = "ssh"
+    agent = false
+
+    host        = element(flatten(clouddk_server.master_node[0].network_interface_addresses), 0)
+    port        = 22
+    user        = "root"
+    private_key = tls_private_key.master_node_ssh.private_key_pem
+    timeout     = "5m"
+  }
+
+  provisioner "file" {
+    content     = <<EOT
+apiVersion: v1
+kind: Secret
+metadata:
+  name: clouddk-cloud-controller-manager-config
+  namespace: kube-system
+type: Opaque
+data:
+  CLOUDDK_API_ENDPOINT: 'https://api.cloud.dk/v1'
+  CLOUDDK_API_KEY: '${var.provider_token}'
+  CLOUDDK_SSH_PRIVATE_KEY: '${base64encode(module.load_balancers.load_balancer_ssh_private_key)}'
+  CLOUDDK_SSH_PUBLIC_KEY: '${base64encode(module.load_balancers.load_balancer_ssh_public_key)}'
+EOT
+    destination = "/tmp/clouddk.config.yaml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export KUBECONFIG=/etc/kubernetes/admin.conf",
+      "kubectl apply -f /tmp/clouddk.config.yaml",
+      "rm -f /tmp/clouddk.config.yaml",
+      "kubectl apply -f https://raw.githubusercontent.com/danitso/clouddk-cloud-controller-manager/master/deployment.yaml",
     ]
   }
 }
