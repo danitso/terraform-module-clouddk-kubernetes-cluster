acfa80614438e29824d0175748a6848445cbbdb6
Dan Petersen <dp@danitso.com>
2019-08-11 14:53:36 +0200

    Latest changes

--- modules/nodes/keys.resources.tf
+++ modules/nodes/keys.resources.tf
@@ -2,12 +2,12 @@
 # STEP 1: Generate SSH key pair
 #===============================================================================
 resource "local_file" "private_ssh_key" {
-  filename          = "${path.root}/keys/id_rsa_${local.node_type}_${replace(var.cluster_name, "/[^A-Za-z0-9]+/", "_")}"
+  filename          = "${path.root}/keys/id_rsa_${local.node_type}_${replace(var.cluster_name, "/[^A-Za-z0-9]+/", "_")}_${replace(var.node_pool_name, "/[^A-Za-z0-9]+/", "_")}"
   sensitive_content = tls_private_key.private_ssh_key.private_key_pem
 }
 
 resource "local_file" "public_ssh_key" {
-  filename = "${path.root}/keys/id_rsa_${local.node_type}_${replace(var.cluster_name, "/[^A-Za-z0-9]+/", "_")}.pub"
+  filename = "${path.root}/keys/id_rsa_${local.node_type}_${replace(var.cluster_name, "/[^A-Za-z0-9]+/", "_")}_${replace(var.node_pool_name, "/[^A-Za-z0-9]+/", "_")}.pub"
   content  = tls_private_key.private_ssh_key.public_key_openssh
 }
 
